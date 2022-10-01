65e90589c7bc10f57717ceb77cdbcd8363d8ecae
Charan <itscharanteja789@gmail.com>
2022-10-01 17:44:16 +0530

    First commit on 1st Oct

--- main.java
+++ main.java
@@ -1,5 +1,8 @@
 public class main{
+    
     public static void main(String[] args){
+        String name = "";
+        System.out.println(name.isEmpty());
         System.out.println("Hello Mac");
     }
 }
