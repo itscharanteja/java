709e7d351c04f3c6da3b08e732c18d5cbf2bdc03
Charan <itscharanteja789@gmail.com>
2022-10-06 10:12:17 +0530

    New Commit

--- main.java
+++ main.java
@@ -1,9 +1,10 @@
 public class main{
     
     public static void main(String[] args){
-        String name = "Not empty";
-        System.out.println(name.isEmpty());
-        System.out.println("Hello Mac");
+        long a = 30;
+        long b = a;
+        
+        System.out.println(b);
     }
 
     
