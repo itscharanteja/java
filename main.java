e95abd3c08e747a785bd455b1a13b08bcf32e58f
Charan <itscharanteja789@gmail.com>
2022-10-07 21:04:08 +0530

    Changes in files

--- main.java
+++ main.java
@@ -1,9 +1,12 @@
+import java.lang.reflect.Type;
+import java.sql.Date;
+
 public class main{
     
     public static void main(String[] args){
         long a = 30;
         long b = a;
-        
+        Date now = new Date();
         System.out.println(b);
     }
 
