41d3bc2208327746b5232e85fcd7fada66e56783
Charan <itscharanteja789@gmail.com>
2022-10-09 16:53:33 +0530

    changes

--- main.java
+++ main.java
@@ -2,7 +2,7 @@
 public class main{
     
     public static void main(String[] args){
-        long a = 30;
+        long a = 330;
         long b = a;
         //Date now = new Date();
         System.out.println(b);
