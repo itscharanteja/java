1fdb0e21c888b96bf46fcbebf7fbe5259466a0dc
Charan <itscharanteja789@gmail.com>
2022-11-03 07:28:33 +0530

    NEW FILES IN JAVA

---
+++ const_overload.java
@@ -0,0 +1,26 @@
+public class const_overload {  
+    //instance variables of the class  
+    int id;  
+    String name;  
+      
+    const_overload(){  
+    System.out.println("this a default constructor");  
+    }  
+      
+    const_overload(int i, String n){  
+    id = i;  
+    name = n;  
+    }  
+      
+    public static void main(String[] args) {  
+    //object creation  
+    const_overload s = new const_overload();  
+    System.out.println("\nDefault Constructor values: \n");  
+    System.out.println("Student Id : "+s.id + "\nStudent Name : "+s.name);  
+      
+    System.out.println("\nParameterized Constructor values: \n");  
+    const_overload student = new const_overload(10, "David");  
+    System.out.println("Student Id : "+student.id + "\nStudent Name : "+student.name);  
+    }
+}  
+    
