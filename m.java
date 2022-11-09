79a48f02ef1c59dde62311582c9b2f694d6d2569
Charan <itscharanteja789@gmail.com>
2022-11-09 21:41:29 +0530

    9-nov

--- m.java
+++ m.java
@@ -1,12 +1,18 @@
 public class m{
-    
-    public static void main(String[] args){
-        long a = 330;
-        long b = a;
-        System.out.println(b);
-        System.out.println("HEllo World");
+    int sum(int x,int y){
+        return x+y;
     }
+    int sum(int x,int y,int z){
+        return x+y+z;
 
+    }
+    double sum(double x,double y){
+        return x+y;
+    }
+    public static void main(String args[]){
+        m o = new m();
+        System.out.println(o.sum(3, 6));
+        System.out.println(o.sum(3, 5, 7));
+        System.out.println(o.sum(3.6, 4.7));
+    }
 }
-
-    
---
+++ polymorphism.java
@@ -0,0 +1,38 @@
+class A{
+    void callme(){
+        System.out.println("Inside A");
+
+
+    }
+}
+
+class B extends A{
+    void callme(){
+        System.out.println("Inside B");
+
+    }
+
+}
+
+class C extends A{
+    void callme(){
+        System.out.println("Inside C");
+
+    }
+}
+
+class polymorphism{
+    public static void main(String args[]){
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A r;
+        r = a;
+        r.callme();
+        r = b;
+        r.callme();
+        r = c;
+        r.callme();
+
+    }
+}
